3fce62546d9de459b04ee39d7b62f488b913e285
AaronMacF <aaron.j.macfarlane2@gmail.com>
2024-12-14 11:32:39 +0000

    Run black formatting on python I've become so typescript-y :O

--- python/src/days/day5/day5_part2.py
+++ python/src/days/day5/day5_part2.py
@@ -6,96 +6,106 @@ from utils.file_utilities import get_root_filepath
 # Dictionary where key is page number, value is set of page numbers that CAN'T appear after it
 PageRules = DefaultDict[int, Set[int]]
 
-def run_day_5_part_2():
-  filepath: str = get_root_filepath()
 
-  with open(filepath + 'day5/page_ordering.txt', 'r') as file:
-    middle_page_numbers = sum_middle_pages_of_corrected_updates(file)
-    print(f'Sum of middle page numbers is {middle_page_numbers}')
-    
+def run_day_5_part_2():
+    filepath: str = get_root_filepath()
+
+    with open(filepath + "day5/page_ordering.txt", "r") as file:
+        middle_page_numbers = sum_middle_pages_of_corrected_updates(file)
+        print(f"Sum of middle page numbers is {middle_page_numbers}")
+
 
 def sum_middle_pages_of_corrected_updates(file) -> int:
-  file_contents: str = file.read()
+    file_contents: str = file.read()
 
-  # split into two sections. section 1 = list of page ordered rules, section 2 = page numbers updates
-  sections: List[str] = file_contents.split("\n\n")
-  assert len(sections) == 2
+    # split into two sections. section 1 = list of page ordered rules, section 2 = page numbers updates
+    sections: List[str] = file_contents.split("\n\n")
+    assert len(sections) == 2
 
-  rules_raw: List[str] = sections[0].splitlines()
-  updates_raw: List[str] = sections[1].splitlines()
+    rules_raw: List[str] = sections[0].splitlines()
+    updates_raw: List[str] = sections[1].splitlines()
 
-  page_rules = get_page_rules(rules_raw)
-  updates: List[List[int]] = get_updates(updates_raw)
+    page_rules = get_page_rules(rules_raw)
+    updates: List[List[int]] = get_updates(updates_raw)
 
-  total: int = 0
-  for update in updates:
-    total += get_middle_page_number_of_corrected_updates(update, page_rules)
+    total: int = 0
+    for update in updates:
+        total += get_middle_page_number_of_corrected_updates(update, page_rules)
 
-  return total
+    return total
 
 
 def get_page_rules(rules_raw: List[str]) -> PageRules:
-  page_rules: PageRules = defaultdict(set) # Value is set of pages that CAN'T appear after key
-  for rule in rules_raw:
-    (page_1_str, page_2_str) = rule.split('|')
-    (page_1, page_2) = int(page_1_str), int(page_2_str)
+    page_rules: PageRules = defaultdict(
+        set
+    )  # Value is set of pages that CAN'T appear after key
+    for rule in rules_raw:
+        (page_1_str, page_2_str) = rule.split("|")
+        (page_1, page_2) = int(page_1_str), int(page_2_str)
 
-    page_rules.append((page_1, page_2))
-  return page_rules
+        page_rules.append((page_1, page_2))
+    return page_rules
 
 
-def get_middle_page_number_of_corrected_updates(update: List[str], page_rules: PageRules) -> int:
-  # If update is already valid, return 0
-  # If update is not valid, correct it and return the middle page number
-  valid_update: List[int] = update.copy()
-  is_invalid = False
+def get_middle_page_number_of_corrected_updates(
+    update: List[str], page_rules: PageRules
+) -> int:
+    # If update is already valid, return 0
+    # If update is not valid, correct it and return the middle page number
+    valid_update: List[int] = update.copy()
+    is_invalid = False
 
-  i = 0
-  while i < len(valid_update):
-    current_page = valid_update[i]
+    i = 0
+    while i < len(valid_update):
+        current_page = valid_update[i]
 
-    # Get the page numbers after the current page
-    next = valid_update[i+1:]
+        # Get the page numbers after the current page
+        next = valid_update[i + 1 :]
 
-    # Get all the page numbers that shouldn't appear after this current one
-    invalid_following_pages = page_rules[current_page]
+        # Get all the page numbers that shouldn't appear after this current one
+        invalid_following_pages = page_rules[current_page]
 
-    # If there are no invalid pages, current page is valid so move on to the next one
-    if len(set(next).intersection(invalid_following_pages)) == 0:
-      i += 1
-      continue
+        # If there are no invalid pages, current page is valid so move on to the next one
+        if len(set(next).intersection(invalid_following_pages)) == 0:
+            i += 1
+            continue
 
-    # If there is at least one invalid page, move the current page after the largest one
-    for page in reversed(list(invalid_following_pages)):
-      if page not in next:
-        continue
-      is_invalid = True
+        # If there is at least one invalid page, move the current page after the largest one
+        for page in reversed(list(invalid_following_pages)):
+            if page not in next:
+                continue
+            is_invalid = True
 
-      # Move current page to be immediately after the largest invalid page
-      invalid_page_index = valid_update.index(page)
-      valid_update.insert(invalid_page_index + 1, current_page)
-      valid_update.pop(i)
+            # Move current page to be immediately after the largest invalid page
+            invalid_page_index = valid_update.index(page)
+            valid_update.insert(invalid_page_index + 1, current_page)
+            valid_update.pop(i)
 
-      # Once this is done for the largest invalid number, stop
-      break
+            # Once this is done for the largest invalid number, stop
+            break
 
-  if (is_invalid):
-    return get_middle_page_number(valid_update)
-  else:
-    return 0
+    if is_invalid:
+        return get_middle_page_number(valid_update)
+    else:
+        return 0
 
 
 def get_updates(updates_raw: List[str]) -> List[List[int]]:
-  return [[int(update_page) for update_page in update.split(',')] for update in updates_raw ]
-  
+    return [
+        [int(update_page) for update_page in update.split(",")]
+        for update in updates_raw
+    ]
+
 
 def get_middle_page_number(update: List[int]) -> int:
-  if (len(update) % 2 == 0):
-    raise ValueError("Instructions not clear what to do for an even number of pages in an update")
-  
-  middle = floor(len(update) / 2)
-  return update[middle]
+    if len(update) % 2 == 0:
+        raise ValueError(
+            "Instructions not clear what to do for an even number of pages in an update"
+        )
+
+    middle = floor(len(update) / 2)
+    return update[middle]
 
 
-if (__name__ == '__main__'):
-  run_day_5_part_2()
+if __name__ == "__main__":
+    run_day_5_part_2()
--- python/src/days/day6/day6_part1.py
+++ python/src/days/day6/day6_part1.py
@@ -3,91 +3,104 @@ from utils.file_utilities import get_root_filepath
 
 Coordinates = Tuple[int, int]
 
+
 class Map:
-  direction: str
-  positions: List[str]
-  current_position: Coordinates
+    direction: str
+    positions: List[str]
+    current_position: Coordinates
 
-  def __init__(self, positions: List[str]):
-    self.positions = positions
-    self.current_position = Map.find_current_position(positions)
-    self.set_position_as_visited(self.current_position)
-    self.direction = 'up'
+    def __init__(self, positions: List[str]):
+        self.positions = positions
+        self.current_position = Map.find_current_position(positions)
+        self.set_position_as_visited(self.current_position)
+        self.direction = "up"
 
-  @staticmethod
-  def create_map_from_filepath(filepath: str) -> 'Map':
-    with open(filepath, 'r') as file:
-      positions = file.read().splitlines()
-      return Map(positions)
+    @staticmethod
+    def create_map_from_filepath(filepath: str) -> "Map":
+        with open(filepath, "r") as file:
+            positions = file.read().splitlines()
+            return Map(positions)
 
-  @staticmethod
-  def find_current_position(positions: List[List[str]]) -> Coordinates:
-    for (i, row) in enumerate(positions):
-      if '^' in row:
-        return (i, row.index('^'))
-    
-    raise ValueError("No caret found in positions given")
+    @staticmethod
+    def find_current_position(positions: List[List[str]]) -> Coordinates:
+        for i, row in enumerate(positions):
+            if "^" in row:
+                return (i, row.index("^"))
 
-  def get_distinct_positions_visited(self) -> int:
-    visited_positions_per_row = [row.count('X') for row in self.positions]
-    return sum(visited_positions_per_row)
+        raise ValueError("No caret found in positions given")
 
-  def simulate_guard(self) -> None:
-    while (True):
-      if self.move_to_next_obstacle() is None:
-        return
-      self.change_direction()
+    def get_distinct_positions_visited(self) -> int:
+        visited_positions_per_row = [row.count("X") for row in self.positions]
+        return sum(visited_positions_per_row)
 
-  def change_direction(self) -> None:
-    match self.direction:
-      case 'up':
-        self.direction = 'right'
-      case 'right':
-        self.direction = 'down'
-      case 'down':
-        self.direction = 'left'
-      case 'left':
-        self.direction = 'up'
+    def simulate_guard(self) -> None:
+        while True:
+            if self.move_to_next_obstacle() is None:
+                return
+            self.change_direction()
 
-  def move_to_next_obstacle(self) -> Coordinates | None:
-    # finds the co-ordinates of the next obstacle, marking each position before then as visisted
-    positions_ahead = self.get_positions_ahead_of_current()
-    for position in positions_ahead:
-      (row, col) = position
-      if self.positions[row][col] == '#':
-        return position
-      self.set_position_as_visited(position)
-      self.current_position = position
-    
-    # no obstacles found in positions ahead
-    return None
+    def change_direction(self) -> None:
+        match self.direction:
+            case "up":
+                self.direction = "right"
+            case "right":
+                self.direction = "down"
+            case "down":
+                self.direction = "left"
+            case "left":
+                self.direction = "up"
 
-  def set_position_as_visited(self, position: Coordinates) -> None:
-    (row, col) = position
-    if self.positions[row][col] == '#':
-      raise ValueError(f"Tried to set an obstacle as visisted at position ({row}, {col})")
-    self.positions[row] = self.positions[row][:col] + 'X' + self.positions[row][col + 1:]
+    def move_to_next_obstacle(self) -> Coordinates | None:
+        # finds the co-ordinates of the next obstacle, marking each position before then as visisted
+        positions_ahead = self.get_positions_ahead_of_current()
+        for position in positions_ahead:
+            (row, col) = position
+            if self.positions[row][col] == "#":
+                return position
+            self.set_position_as_visited(position)
+            self.current_position = position
 
-  def get_positions_ahead_of_current(self) -> List[Coordinates]:  
-    (row, col) = self.current_position
-    match self.direction:
-      case 'up':
-        return [(i, col) for i in reversed(range(row))]
-      case 'right':
-        return [(row, i) for i in range(col + 1, col + len(self.positions[row][col + 1:]) + 1)]
-      case 'down':
-        return [(i, col) for i in range(row + 1, len(self.positions))]
-      case 'left':
-        return [(row, i) for i in reversed(range(len(self.positions[row][:col])))]
+        # no obstacles found in positions ahead
+        return None
+
+    def set_position_as_visited(self, position: Coordinates) -> None:
+        (row, col) = position
+        if self.positions[row][col] == "#":
+            raise ValueError(
+                f"Tried to set an obstacle as visisted at position ({row}, {col})"
+            )
+        self.positions[row] = (
+            self.positions[row][:col] + "X" + self.positions[row][col + 1 :]
+        )
+
+    def get_positions_ahead_of_current(self) -> List[Coordinates]:
+        (row, col) = self.current_position
+        match self.direction:
+            case "up":
+                return [(i, col) for i in reversed(range(row))]
+            case "right":
+                return [
+                    (row, i)
+                    for i in range(
+                        col + 1, col + len(self.positions[row][col + 1 :]) + 1
+                    )
+                ]
+            case "down":
+                return [(i, col) for i in range(row + 1, len(self.positions))]
+            case "left":
+                return [
+                    (row, i) for i in reversed(range(len(self.positions[row][:col])))
+                ]
 
 
 def run_day_6_part_1():
-  filepath: str = get_root_filepath()
-  filepath = filepath + 'day6/map.txt'
+    filepath: str = get_root_filepath()
+    filepath = filepath + "day6/map.txt"
 
-  map = Map.create_map_from_filepath(filepath)
-  map.simulate_guard()
-  print(map.get_distinct_positions_visited())
+    map = Map.create_map_from_filepath(filepath)
+    map.simulate_guard()
+    print(map.get_distinct_positions_visited())
 
-if (__name__ == '__main__'):
-  run_day_6_part_1()
+
+if __name__ == "__main__":
+    run_day_6_part_1()
